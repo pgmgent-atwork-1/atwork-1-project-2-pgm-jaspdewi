2a79db25fce5201255146846d0550cc7df0e0240
unknown <jasper.dewilde1@student.arteveldehs.be>
2022-01-22 19:44:54 +0100

    Small changes to the data sorting in services.js

--- static/js/services.js
+++ static/js/services.js
@@ -2,11 +2,19 @@ function GetData() {
   this.getEventData = async () => {
       try {
         const response = await fetch(`https://www.pgm.gent/data/gentsefeesten/events_500.json`);
-        const data = await response.json();
-        data.sort((a, b) => {
+        const unsortedData = await response.json();
+        const partiallySortedData = unsortedData.sort((a, b) => {
           if (a.start > b.start) {
             return 1;
           } else if (a.start < b.start) {
+            return -1;
+          }
+          return 0;
+        })
+        const data = partiallySortedData.sort((a, b) => {
+          if (a.day > b.day) {
+            return 1;
+          } else if (a.day < b.day) {
             return -1;
           }
           return 0;
